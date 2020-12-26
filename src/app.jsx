fc4b3924ecc66b76254c037b7e1c73c97c572ab9
mr-yanghm <code004@gmail.com>
2020-12-26 23:33:24 +0900

    react

---
+++ src/app.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./app.css";
+import Habits from './components/habits';
+import Navbar from './components/navbar';
+
+class App extends Component {
+  state = {
+      habits : [
+          { id: 1, name: 'Reading', count:0},
+          { id: 2, name: 'Running', count:0},
+          { id: 3, name: 'Coding', count:0},
+      ]
+  }
+
+  handleIncrement = (habit)=>{
+    // bed coding style in react !!!
+    // habit.count++;
+    // this.setState(this.state);
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({habits : habits});
+}
+handleDecrement = (habit)=>{
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count < 0 ? 0 : count;
+    this.setState({habits});
+}
+handleDelete = (habit)=>{
+    const index = this.state.habits.indexOf(habit);
+    const habits = this.state.habits.filter(_habit => _habit.id !== habit.id );
+    this.setState({habits:habits});
+}
+  render(){
+    return (
+      <>
+        <Navbar totalCount={this.state.habits.filter(item=> item.count > 0).length}/>
+        <Habits 
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    )
+    ;
+  }
+
+}
+
+export default App;
